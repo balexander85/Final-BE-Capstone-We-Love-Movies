22fb618698843c145de024b575e31095842716f6
Brian Alexander <briandanielalexander@gmail.com>
2024-02-14 13:11:56 -0600

    Saving progress with Theaters suite passing

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -2,8 +2,7 @@ const service = require("./theaters.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 async function list(request, response) {
-  // TODO: Add your code here
-  response.json({ });
+  response.json({ data: await service.list() });
 }
 
 module.exports = {
